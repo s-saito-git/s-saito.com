95b42ce5bd6bcf571d7bb372d41e1c1636202c4c
s-saito <s.s.saito108@gmail.com>
2021-03-18 22:34:48 +0900

    home作成

---
+++ src/views/SuutaApp.vue
@@ -0,0 +1,7 @@
+<template>
+	<v-ons-page>
+		<p>
+			suutaappの詳細
+		</p>
+	</v-ons-page>
+</template>
--- src/views/Works.vue
+++ src/views/Works.vue
@@ -1,13 +1,49 @@
 <template>
   <v-ons-page>
-    <Toolbar>Works</Toolbar>
+    <div class="works">
+      <h2>Works</h2>
+      <ul>
+        <li class="works__list" v-for="work in works" :key="work">
+          <img class="works__img" :src="work.img">
+          <h3 class="works__product">
+            {{ work.product }}
+          </h3>
+          <p>
+            {{ work.description }}
+          </p>
+        </li>
+      </ul>
+    </div>
   </v-ons-page>
 </template>
 
 <script>
-import Toolbar from "../components/Toolbar";
-
 export default {
-    components: { Toolbar }
-};
+  data: function() {
+    return {
+      works: [
+        { 
+          img: '/img/suuta_app.jpg',
+          product: 'SUUTA App',
+          description: 'CtoCレンタルプラットフォーム「SUUTA（スータ）」'
+        },
+        { 
+          img: '/img/suuta_app.jpg',
+          product: 'SUUTA App',
+          description: 'CtoCレンタルプラットフォーム「SUUTA（スータ）」'
+        },
+        { 
+          img: '/img/suuta_app.jpg',
+          product: 'SUUTA App',
+          description: 'CtoCレンタルプラットフォーム「SUUTA（スータ）」'
+        },
+        { 
+          img: '/img/suuta_app.jpg',
+          product: 'SUUTA App',
+          description: 'CtoCレンタルプラットフォーム「SUUTA（スータ）」'
+        },
+      ]
+    };
+  }
+}
 </script>
