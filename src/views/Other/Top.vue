99262f09a2e9e1a75941e100c08d4176ae2d6ea4
s-saito <s.s.saito108@gmail.com>
2021-03-31 00:01:27 +0900

    Other > 作成中

--- src/views/Other/Top.vue
+++ src/views/Other/Top.vue
@@ -1,43 +1,55 @@
 <template>
   <v-ons-page>
-    <div class="other">
-      <h2>Other</h2>
+    <div class="bg-gray">
+      <div class="title-wrap">
+        <h2>Other</h2>
+      </div>
+      <v-ons-list class="mb32">
+        <v-ons-list-item>
+          <div class="left">
+            <img class="list-item__thumbnail" src="http://placekitten.com/g/40/40">
+          </div>
+          <div class="center">
+            <span class="list-item__title">Shinsuke Saito</span>
+            <span class="list-item__subtitle">On the Internet</span>
+          </div>
+        </v-ons-list-item>
+      </v-ons-list>
+      <v-ons-list>
+        <v-ons-list-item tappable onClick="window.open('https://github.com/s-saito-git')">
+          <div class="left">
+            <v-ons-icon icon="fa-github" class="list-item__icon"></v-ons-icon>
+          </div>
+          <div class="center">
+            GitHub
+          </div>
+          <div class="right">
+            <v-ons-icon icon="fa-external-link-alt" class="list-item__icon"></v-ons-icon>
+          </div>
+        </v-ons-list-item>
+        <v-ons-list-item tappable onClick="window.open('http://google.jp/')">
+          <div class="left">
+            <v-ons-icon icon="fa-twitter" class="list-item__icon"></v-ons-icon>
+          </div>
+          <div class="center">
+            Twitter
+          </div>
+          <div class="right">
+            <v-ons-icon icon="fa-external-link-alt" class="list-item__icon"></v-ons-icon>
+          </div>
+        </v-ons-list-item>
+        <v-ons-list-item tappable onClick="window.open('mailto:s.s.saito108@gmail.com')">
+          <div class="left">
+            <v-ons-icon icon="fa-envelope" class="list-item__icon"></v-ons-icon>
+          </div>
+          <div class="center">
+            Contact
+          </div>
+          <div class="right">
+            <v-ons-icon icon="fa-external-link-alt" class="list-item__icon"></v-ons-icon>
+          </div>
+        </v-ons-list-item>
+      </v-ons-list>
     </div>
-
-    <v-ons-list>
-      <v-ons-list-item tappable onClick="window.open('https://github.com/s-saito-git')">
-        <div class="left">
-          <v-ons-icon icon="fa-github" class="list-item__icon"></v-ons-icon>
-        </div>
-        <div class="center">
-          GitHub
-        </div>
-        <div class="right">
-          <v-ons-icon icon="fa-external-link-alt" class="list-item__icon"></v-ons-icon>
-        </div>
-      </v-ons-list-item>
-      <v-ons-list-item tappable onClick="window.open('http://google.jp/')">
-        <div class="left">
-          <v-ons-icon icon="fa-twitter" class="list-item__icon"></v-ons-icon>
-        </div>
-        <div class="center">
-          Twitter
-        </div>
-        <div class="right">
-          <v-ons-icon icon="fa-external-link-alt" class="list-item__icon"></v-ons-icon>
-        </div>
-      </v-ons-list-item>
-      <v-ons-list-item tappable onClick="window.open('mailto:s.s.saito108@gmail.com')">
-        <div class="left">
-          <v-ons-icon icon="fa-envelope" class="list-item__icon"></v-ons-icon>
-        </div>
-        <div class="center">
-          Contact
-        </div>
-        <div class="right">
-          <v-ons-icon icon="fa-external-link-alt" class="list-item__icon"></v-ons-icon>
-        </div>
-      </v-ons-list-item>
-    </v-ons-list>
   </v-ons-page>
 </template>
